1be62ff9a2a3018f856d466ddeec7ef0b258ba7f
ZVyachelsavV <vvvv.zkv@gmail.com>
2025-11-24 00:05:50 +0200

    add sidebar transform

--- app/(private routes)/notes/filter/layout.tsx
+++ app/(private routes)/notes/filter/layout.tsx
@@ -1,4 +1,47 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import clsx from 'clsx';
 import css from './LayoutNotes.module.css';
+
+interface Props {
+  children: React.ReactNode;
+  sidebar: React.ReactNode;
+}
+
+const EDGE_ZONE = 30;
+
+const NotesLayout = ({ children, sidebar }: Props) => {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      if (e.clientX <= EDGE_ZONE) setOpen(true);
+      if (e.clientX > 250) setOpen(false);
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => window.removeEventListener('mousemove', handleMouseMove);
+  }, []);
+
+  return (
+    <section className={css.container}>
+      <aside
+        className={clsx(css.sidebar, {
+          [css.sidebarVisible]: open,
+        })}
+      >
+        {sidebar}
+      </aside>
+
+      <div className={css.notesWrapper}>{children}</div>
+    </section>
+  );
+};
+
+export default NotesLayout;
+
+/* import css from './LayoutNotes.module.css';
 
 interface Props {
   children: React.ReactNode;
@@ -12,4 +55,4 @@ const NotesLayout = ({ children, sidebar }: Props) => (
   </section>
 );
 
-export default NotesLayout;
+export default NotesLayout; */
